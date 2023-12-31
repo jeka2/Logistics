1dacc9460a591125d387f4fc1a0a244a93a7bde7
Yevgeniy Ivanov <anigeka@gmail.com>
2023-12-31 10:31:24 -0500

    add initial files

---
+++ Logistics/Logistics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Logistics
+//
+//  Created by Yevgeniy Ivanov on 12/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
